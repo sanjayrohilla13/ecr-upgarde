9c81b7f2684bb93aaf69cbe39b10f18338fe80c7
Sanjay Kumar <Sanjayrohilla@gmail.com>
2021-11-30 22:17:13 +1100

    sanjay

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "Demo_Instance" {
+resource "aws_instance" "Demo_Instance" {   
     #ami = "ami-0c9f90931dd48d1f2"
     ami = "ami-0bf8b986de7e3c7ce"    
     instance_type = "t2.micro"
     #availability_zone = "ap-southeast-2"
     tags = {
-        Name = var.docker_src
+        Name = var.docker_src == 'ECR' ? "ECR-Repo": "artifactory-Repo" 
     }
 }
